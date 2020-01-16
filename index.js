be958e3416b8b4545166f46f84c44bbe536db09e
中村 育豊 <akatsuki.unknow@gmail.com>
2020-01-16 21:28:25 +0900

    Discord追加

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const PORT = process.env.PORT || 5000;
 const line = require("@line/bot-sdk");
+const Discord = require('discord.js');
+const DiscordClient = new Discord.Client();
+const token = process.DISCORD_TOKEN;
 
 const config = {
   channelAccessToken: process.env.ACCESS_TOKEN,
   channelSecret: process.env.SECRET_KEY
 };
-const client = new line.Client(config);
+const LineClient = new line.Client(config);
 
 express()
   .set('views', __dirname + '/pages')
@@ -27,9 +30,24 @@ function lineBot(req, res) {
 }
 
 async function echoman(ev) {
-  const pro =  await client.getProfile(ev.source.userId);
-  return client.replyMessage(ev.replyToken, {
+  const pro =  await LineClient.getProfile(ev.source.userId);
+  return LineClient.replyMessage(ev.replyToken, {
     type: "text",
     text: `${pro.displayName}さん、今「${ev.message.text}」って言いました？`
   });
 }
+
+var myname = "LineDiscord";
+var master = "akatsuki1910";
+
+DiscordClient.on('message', message => {
+    if(message.content === "!test") {
+        author = message.author.username;
+        if (author === master) {
+            var dateStr = new Date().toLocaleString();
+            message.channel.send({embed: {color: 2550000,description: dateStr + "\nThis is test text"}});
+        }
+    }
+});
+
+DiscordClient.login(token);
